c174cb345119691515b337715b01dcf48bd5de97
dinesh2000-1 <113331296+dinesh2000-1@users.noreply.github.com>
2023-02-14 19:44:19 +0530

    if-else

---
+++ ifelse.java
@@ -0,0 +1,10 @@
+public class ifelse {
+    public static void main (String[] args)
+    {
+        int x=9;
+        if (x>=10)
+        System.out.println("true");
+        else
+        System.out.println("flase");
+    }
+}
